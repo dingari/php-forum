a7bf1db2d21198c2e93afcb28b6c90cc2f434f6a
dingari <danielgretars@gmail.com>
2016-08-01 19:34:40 +0000

    inital commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title><?php echo "Hello world";?></title>
+	</head>
+
+	<body>
+		<h1><?php echo "Who let the dogs out";?></h1>
+	</body>
+
+	<p>5 + 3 is <?php echo 5 + 3;?></p>
+</html>
